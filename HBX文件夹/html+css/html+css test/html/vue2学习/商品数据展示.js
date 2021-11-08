00064c586122f12c6c3b71ee02f4ceec1cf4adfa
moon-force的笔记本 <862315743@qq.com>
2021-11-08 21:53:57 +0800

    no message

---
+++ HBX文件夹/html+css/html+css test/html/vue2学习/商品数据展示.js
@@ -0,0 +1,13 @@
+var webstore=new Vue({
+el:".product",
+data:{
+	sitename:"store",
+	product:{
+		id:1001,
+		title:"cat food",
+		description:"cat food very good",
+		price:2000,
+		image:"../../img/猫粮.jpg"
+	}
+}
+})
