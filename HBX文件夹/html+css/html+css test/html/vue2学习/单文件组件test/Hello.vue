57a1e6bcec1773289b3fc4b554e8591025c19cb4
moon-force的笔记本 <862315743@qq.com>
2021-11-21 22:08:52 +0800

    no message

---
+++ HBX文件夹/html+css/html+css test/html/vue2学习/单文件组件test/Hello.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+	    <h1>{{ msg }}</h1>
+	</div>
+</template>
+
+<script>
+	export default {
+	  name: 'hello',
+	  data () {
+	    return {
+	      msg: '欢迎来到菜鸟教程！'
+	    }
+	  }
+	}
+</script>
+
+<style>
+</style>
