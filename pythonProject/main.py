57a05afda2f3eb1eb9f8924d8e51e79194f5e3bf
moon-force的笔记本 <862315743@qq.com>
2022-03-12 21:07:49 +0800

    删除

--- pythonProject/main.py
+++
@@ -1,9 +0,0 @@
-var1 = 5
-var2 = 3
-var3 = 12
-var4 = 43
-var1 = var1 << 2
-result = (var1 + var2) * var3
-print(result)
-result = ((var1 & var2) + var3) * var4
-print(result)
--- 通讯录/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-/**
- * @author chen
- */
-public class Main {
-    public static void main(String[] args){
-     Run.run();
-    }
-}
